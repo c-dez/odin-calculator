053209674c34d9250d6d291be0a91f464542fa9b
c-dez <hdezcruzcarlos@gmail.com>
2023-06-06 21:20:28 -0600

    added labels for allNumBtn

--- app.js
+++ app.js
@@ -25,18 +25,57 @@ btnContainer.style.height = '80%'
 btnContainer.style.margin = '5px';
 btnContainer.style.border = '1px solid black'
 
-//buttons numbers
+//buttons numbers numbersGrid
 const numbersGrid = document.createElement('div');
 btnContainer.appendChild(numbersGrid);
 numbersGrid.classList.add('numbersGrid')
+numbersGrid.style.display='flex'
+numbersGrid.style.flexWrap= 'wrap'
+// numbersGrid.style.flexDirection = 'row-reverse'
+numbersGrid.style.justifyContent = 'center';
+
 
 //function creates num number of buttons
 function createButtons(num,father){
-for(let i =0; i< num; i++){
+for(let i =0; i<num; i++){
     const btn = document.createElement('button');
-    btn.classList.add('btn'+[i])
+    // btn.id=('btn'+[i]);
+    btn.textContent = `${[i]}`;
     father.appendChild(btn);
 }
-
 }
-createButtons(10,numbersGrid);
+createButtons(12,numbersGrid);
+//btn 0-9
+const allNumBtn =Array.from( numbersGrid.querySelectorAll('button'));
+numbersGrid.id='numbersGrid';
+
+
+//asigna tama;o a cada button de allNumBtn
+allNumBtn.forEach(item =>{
+    item.style.width='30%'
+    item.style.height='40px'
+
+})
+//array de objetos
+
+
+// function CreateBtnObj(symbol){
+//     this.symbol = symbol;
+//     this.classList = `btn${symbol}`;
+//     this.behavior =()=>{
+
+//     }
+// }
+ allNumBtn[0].textContent =7;
+ allNumBtn[1].textContent =8;
+ allNumBtn[2].textContent =9;
+ allNumBtn[3].textContent =4;
+ allNumBtn[4].textContent =5;
+ allNumBtn[5].textContent =6;
+ allNumBtn[6].textContent =1;
+ allNumBtn[7].textContent =2;
+ allNumBtn[8].textContent =3;
+ allNumBtn[9].textContent =0;
+ allNumBtn[10].textContent ='.';
+ allNumBtn[11].textContent ='+/-';
+
