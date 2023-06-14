63497ae82ebf3d48d8713ce08044c692ebd0e298
c-dez <hdezcruzcarlos@gmail.com>
2023-06-13 21:57:08 -0600

    basic event listeners

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const display = document.querySelector('.display')
+const operands = document.querySelectorAll('#operand')
+const operator = document.querySelectorAll('#operator')
+const clearBtn = document.querySelector('#clear')
+
+
+clearBtn.addEventListener('mousedown',()=>{
+    display.textContent = '';
+})
+
+operands.forEach((button)=>{
+        button.addEventListener('mousedown',()=>console.log(button.textContent))
+})
